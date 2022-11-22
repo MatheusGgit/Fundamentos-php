2ff225adbe875f04e6840ad27b97c13c691fdff8
Mathe <garciamatheus27@gmail.com>
2022-11-22 16:16:18 -0300

    mb_string

--- PHP Strings/tamanho.php
+++ PHP Strings/tamanho.php
@@ -1,11 +1,9 @@
 <?php
-// todo: extensão do MBSTRING não ta funcionando?
-
 $string = 'ábç';
 echo strlen($string) . PHP_EOL; // A quantidade que o strlen conta é em BYTE, ou seja, acentos gastam mais de 1 byte
+echo mb_strlen($string) . PHP_EOL; // O mb_strlen funciona de forma normal
+
 echo strtoupper($string).PHP_EOL; // Não lida com acentos
 echo strtolower($string).PHP_EOL;
-
-echo mb_strlen($string) . PHP_EOL; // O mb_strlen funciona de forma normal
-echo mb_strtolower($string).PHP_EOL;
+echo mb_strtoupper($string).PHP_EOL;
 echo mb_strtolower($string).PHP_EOL;
